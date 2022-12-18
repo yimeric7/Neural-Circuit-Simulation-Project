16bd28eccb67b7507ed063e85dfc2b78e944da0d
yimeric7 <112520475+yimeric7@users.noreply.github.com>
2022-12-18 15:49:59 -0500

    Initial commit

---
+++ Neural-Circuit-Simulation-Project/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import * as THREE from 'three';
+import Brain from '../brain-main/Brain.jsx'
+
+function App() {
+    return (
+        <div>
+            <Brain />
+        </div>
+    );
+}
+
+export default App;

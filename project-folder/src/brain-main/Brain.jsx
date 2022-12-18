908463a2dfcae6f99ed5c492eaf589a27c45e849
yimeric7 <112520475+yimeric7@users.noreply.github.com>
2022-12-18 16:58:18 -0500

    changed directory name

---
+++ project-folder/src/brain-main/Brain.jsx
@@ -0,0 +1,38 @@
+import { useEffect } from 'react';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import SceneInit from '../utils/SceneInit';
+
+export default function Brain() {
+    useEffect(() => {
+        const test = new SceneInit('brainCanvas');
+        test.initialize();
+        test.animate();
+
+        let loadedModel;
+        const brainLoader = new GLTFLoader();
+        brainLoader.load('src/brain-model/scene.gltf', (gltfScene) => {
+            loadedModel = gltfScene;
+
+            gltfScene.scene.rotation.y = Math.PI / 8;
+            gltfScene.scene.position.y = 0;
+            gltfScene.scene.scale.set(35, 35, 35);
+            test.scene.add(gltfScene.scene);
+        });
+
+        // const animate = () => {
+        //     if (loadedModel) {
+        //         loadedModel.scene.rotation.x += 0.01;
+        //         loadedModel.scene.rotation.y += 0.01;
+        //         loadedModel.scene.rotation.z += 0.01;
+        //     }
+        //     requestAnimationFrame(animate);
+        // };
+        // animate();
+    }, []);
+
+    return (
+        <div>
+            <canvas id="brainCanvas"/>
+        </div>
+    );
+}
---
+++ project-folder/src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Brain from '../brain-main/Brain.jsx'
+
+export default function App() {
+    return (
+        <div>
+            <Brain />
+        </div>
+    );
+}

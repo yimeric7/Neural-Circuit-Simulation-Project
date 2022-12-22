28506d0f3eed25bcaf8ad8c90afcb8dc3410562b
yimeric7 <112520475+yimeric7@users.noreply.github.com>
2022-12-21 22:39:11 -0500

    fixed footer

--- project-folder/src/pages/Home.jsx
+++ project-folder/src/pages/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import UserNavbar from '../components/Navbar.jsx'
 import Brain from '../brain-main/Brain.jsx'
+import Footer from "../components/Footer.jsx";
 
 export default function Home() {
 
     return (
         <>
             <UserNavbar />
-            <Brain />
+            {/*<Brain />*/}
+            <Footer />
         </>
     )
 }

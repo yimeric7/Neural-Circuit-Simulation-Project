c45feaee3aecdce8327001ebe13fa316733e9168
malrobaie <alrobaiemohammad@gmail.com>
2022-12-22 23:00:07 -0600

    attempt to separate brain

--- project-folder/src/brain-main/Brain.jsx
+++ project-folder/src/brain-main/Brain.jsx
@@ -1,4 +1,5 @@
-import { useEffect } from 'react';
+import { Children, useEffect } from 'react';
+import { MeshDistanceMaterial } from 'three';
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import SceneInit from '../utils/SceneInit';
@@ -8,15 +9,33 @@ export default function Brain() {
     useEffect(() => {
         test.initialize();
         test.animate();
+        
 
         let loadedModel;
         const brainLoader = new GLTFLoader();
         brainLoader.load('src/brain-model/brain.gltf', (gltfScene) => {
+
+
             loadedModel = gltfScene;
-            gltfScene.scene.rotation.x = Math.PI / 8;
-            gltfScene.scene.position.y = 0;
-            gltfScene.scene.scale.set(1, 1, 1);
-            test.scene.add(gltfScene.scene);
+            const model = loadedModel.scene;
+            const meshArr = model.children;
+
+            model.rotation.x = Math.PI / 8;
+            model.position.y = 0;
+            model.scale.set(1, 1, 1);
+           // model.MeshDistanceMaterial = 10
+            test.scene.add(model);
+            console.log(model)
+
+          //  model.getObjectByName("Cerebellum").material.color.setHex(0x00ff00)
+          meshArr.forEach(object => {
+          // object.material.color.setHex(Math.random()*0xffffff)
+           // object.position.y = 10
+           //object.customDistanceMaterial.polygonOffsetUnits(10)
+           //object.customDistanceMaterial.polygonOffset(10)
+          // object.customDistanceMaterial.polygonOffsetFactor(10)
+          })
+        
         });
 
         const animate = () => {
--- project-folder/src/pages/Home.jsx
+++ project-folder/src/pages/Home.jsx
@@ -9,7 +9,7 @@ export default function Home() {
         <>
             <UserNavbar />
             <Brain />
-            {/*<Footer />*/}
+            {/* <Footer /> */}
         </>
     )
 }
